c9c37522d349abb1ee74aec9667e2b42b8ff586a
Nishant Khanna <nishantkhanna87@gmail.com>
2018-05-15 12:50:22 +0530

    no message

---
+++ data_preprocessing/data_preprocessing.R
@@ -0,0 +1,10 @@
+dataset = read.csv("C:\\Users\\nkhanna\\Desktop\\Machine_Learning_AZ_Template_Folder\\Part 1 - Data Preprocessing\\Data.csv")
+
+#Handling Missing Data
+dataset$Age = ifelse(is.na(dataset$Age),
+                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Age)
+
+dataset$Salary = ifelse(is.na(dataset$Salary),
+                     ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Salary)
