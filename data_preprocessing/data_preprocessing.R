d4c93e279507200698fdd0c7aa5ecd004b8a8334
Nishant Khanna <nishantkhanna87@gmail.com>
2018-05-21 11:42:03 +0530

    no message

--- data_preprocessing/data_preprocessing.R
+++ data_preprocessing/data_preprocessing.R
@@ -6,5 +6,15 @@ dataset$Age = ifelse(is.na(dataset$Age),
                      dataset$Age)
 
 dataset$Salary = ifelse(is.na(dataset$Salary),
-                     ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
-                     dataset$Salary)
+                        ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+                        dataset$Salary)
+
+#Encoding Categorical data
+
+dataset$Country = factor(dataset$Country,
+                         levels = c('France', 'Spain', 'Germany'),
+                         labels = c(1, 2, 3))
+
+dataset$Purchased = factor(dataset$Purchased,
+                         levels = c('Yes', 'No'),
+                         labels = c(1, 0))
